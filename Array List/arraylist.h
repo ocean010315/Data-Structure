d1503deb1da703a1da70a6ff3530143dde435d28
오수현 <91974779+ocean010315@users.noreply.github.com>
2022-04-28 11:16:42 +0900

    Create arraylist.h

---
+++ Array List/arraylist.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#define MaxSize 3
+#define boolean unsigned char
+#define true 1
+#define false 0
+typedef char Element;
+
+// Global로 선언한 List 자료구조
+Element List[MaxSize];
+int size = 0;
+
+void list_insert(Element e);
+void list_delete(Element e);
+boolean list_empty();
+boolean list_full();
+void list_show();
